683dc353cf39d0a92a93561fd9bad636b8271a01
Cláudio Roberto <claudioroberto0707@outlook.com>
2026-05-25 22:34:18 -0300

    feat: implement Project REst controller

--- src/main/java/com/portfolio/api/controller/ProjectController.java
+++ src/main/java/com/portfolio/api/controller/ProjectController.java
@@ -3,23 +3,27 @@ package com.portfolio.api.controller;
 import com.portfolio.api.domain.Project;
 import com.portfolio.api.repository.ProjectRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
-@RequestMapping("api/projects")
+@RestController
+@RequestMapping("/projects")
 public class ProjectController {
     private final ProjectRepository repository;
 
     public ProjectController(ProjectRepository repository) {
         this.repository = repository;
     }
+
     @GetMapping
-    public ResponseEntity<List<Project>> create(){
+    public ResponseEntity<List<Project>> listAll(){
         return ResponseEntity.ok(repository.findAll());
     }
 
+    @PostMapping
+    public ResponseEntity<Project> create(@RequestBody Project newProject){
+        Project savedProject = repository.save(newProject);
+            return ResponseEntity.status(201).body(savedProject);
+    };
 }
